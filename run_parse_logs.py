12a177a31314c78e3c64e9120c8d4e71f42bcbd3
Amanda Kepley <akepley@nrao.edu>
2023-01-17 11:36:06 -0500

    initial go at parsing pipeline casalogs

---
+++ run_parse_logs.py
@@ -0,0 +1,5 @@
+from importlib import reload
+
+import parse_logs
+
+parse_logs.parse_pipe_casalog('/Users/akepley/Dropbox/Support/naasc/WSU/big_cubes/data/felix_logfiles/Cycle7/logfiledirectory/member.uid___A001_X3360_X28.hifa_calimage.weblog.tgz/casa-20221005-173548286714693.log.gz')
